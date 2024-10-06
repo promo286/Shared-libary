d7629533ce03c9ae6fd5c01fed29639f95341acb
promo286 <ravipramoth28@gmail.com>
2024-10-06 20:37:37 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,16 +1,1 @@
-def checkoutGitRepo(String branch = 'master', String repoUrl) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        extensions: [],
-        userRemoteConfigs: [[url: repoUrl]]
-    ])
-}
 
-def runTests() {
-    sh 'mvn test'
-}
-
-def buildMavenProject() {
-    sh 'mvn package'
-}
